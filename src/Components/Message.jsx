21bdd63d8c6d66c44272e4fb3f88a6b5c111ac8b
Enes Yağmur <enesyagmur99@gmail.com>
2024-02-16 14:45:56 +0300

    sidebar componetlerinin style işlemleri yapıldı

--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -2,7 +2,18 @@ import React from "react";
 import "./message.scss";
 
 const Message = () => {
-  return <div>Message</div>;
+  return (
+    <div className="message">
+      <img
+        src="https://images.pexels.com/photos/428364/pexels-photo-428364.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        alt=""
+      />
+      <div className="user-info">
+        <span>Berkay Kamberoğlu</span>
+        <p>Selam</p>
+      </div>
+    </div>
+  );
 };
 
 export default Message;
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import "./sidebar.scss";
 import Navbar from "./Navbar";
+import Search from "./Search";
+import Message from "./Message";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <Navbar />
+      <Search />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
     </div>
   );
 };
