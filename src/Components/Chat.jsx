6d3979d7854dcaf804df354e2628b0f65c124e7c
Enes Yağmur <enesyagmur99@gmail.com>
2024-02-16 14:55:22 +0300

    react icons kurularak chat componentinin style işlemlerine başlandı

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "./chat.scss";
+import { BsCameraVideoFill } from "react-icons/bs";
+import { IoMdMore } from "react-icons/io";
 
 const Chat = () => {
-  return <div className="chat">Chat</div>;
+  return (
+    <div className="chat">
+      <div className="chat-info">
+        <p>Berkay</p>
+        <div className="chat-icons">
+          <BsCameraVideoFill />
+          <IoMdMore />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
--- src/Components/Chats.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./chats.scss";
-
-const Chats = () => {
-  return <div>Chats</div>;
-};
-
-export default Chats;
