ca5c5923edd0f4fa9073b1aeed8436c38c33e5e0
erain <enesyagmur99@gmail.com>
2025-01-25 12:02:45 +0300

    api değişiklikleri

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC2Jm0rGW-JRMpdcOdvQDEPZwHp2dhejf4",
-  authDomain: "whatsapp-web-clone-cb385.firebaseapp.com",
-  projectId: "whatsapp-web-clone-cb385",
-  storageBucket: "whatsapp-web-clone-cb385.appspot.com",
-  messagingSenderId: "363136109258",
-  appId: "1:363136109258:web:f33b7defcd86d223b00233",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 export const app = initializeApp(firebaseConfig);
